0f8d660a7d2a4e744d35d9bd489e606989e8ae9b
Alexhwa <ah.alswad@gmail.com>
2021-09-05 23:33:47 -0500

    chaser improvements

--- Outgrown/Assets/Scripts/Enemies/Chaser.cs
+++ Outgrown/Assets/Scripts/Enemies/Chaser.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Chaser : MonoBehaviour
 {
+    //What the chaser moves towards
     [SerializeField] private Transform target;
+    //Defines what objects the chaser will chase
     [SerializeField] private string[] targetTagMask;
+    //How fast the chaser accelerates towards the target
     [SerializeField] private float acceleration;
+    //Max speed before the chaser stops running toward the target
     [SerializeField] private float maxSpeed;
 
     private Rigidbody2D rb;
@@ -35,12 +39,14 @@ public class Chaser : MonoBehaviour
         {
             if (Mathf.Abs(rb.velocity.x) < maxSpeed)
             {
+                //flip chaser if target is behind it
                 if(((target.position - transform.position).x < 0 && transform.localScale.x > 0) || ((target.position - transform.position).x > 0 && transform.localScale.x < 0))
                 {
                     var flipScale = transform.localScale;
                     flipScale.x *= -1;
                     transform.localScale = flipScale;
                 }
+                //accelerate toward target
                 rb.velocity += new Vector2((target.position - transform.position).x, 0).normalized * Time.deltaTime *
                                acceleration;
             }
