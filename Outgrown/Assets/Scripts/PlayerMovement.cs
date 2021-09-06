4331d83c665600eb83029dd9c694aaabc7b2ad22
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2021-09-05 22:11:27 -0500

    First draft of PlayerMovement
    Not fully functional due to a few areas, but mostly completed

--- Outgrown/Assets/Scripts/PlayerMovement.cs
+++ Outgrown/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//	TODO: test these variables
+float accelFactor = 1;
+float gravity = 9.8;
+Vector2 veloc = new Vector2();
+float jumpForce = 1;
+
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,6 +19,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //  analyze left/right input
+        int horzInput = int(Input.GetKey("right")) - int(Input.GetKey("left"));
+        //	apply lerp: accel/decel
+		veloc.x = lerp(veloc.x, horzInput, Time.deltaTime);
+		
+		//	jump
+		if(/* we're on the ground or on an edge */) {
+			if(Input.GetKey("jump") {
+				veloc.y = jumpForce;
+			} else if(veloc.y != 0) {
+				veloc.y = 0;
+			}
+		}
+		//	fall
+		else {
+			veloc.y -= gravity * Time.deltaTime;
+		}
+		
+		//	hit wall or ceiling
+		if(/* collide with wall */) {
+			veloc.x = 0;
+		}
+		if(/* collide with ceiling */ && veloc.y != 0) {
+			veloc.y = 0;
+		}
+		
+        //  update position
+		this.position += veloc;
+		
     }
 }
