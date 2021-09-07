757a177611c32d3f8156126a829e5cea6366f1d4
Alexhwa <ah.alswad@gmail.com>
2021-09-07 12:56:27 -0500

    added new input system

--- Outgrown/Assets/Scripts/PlayerMovement.cs
+++ Outgrown/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	//	TODO: test these variables
-	float accelFactor = 1;
-	float gravity = 9.8f;
-	Vector2 veloc = new Vector2();
-	float jumpForce = 1;
+	[SerializeField] private float accelFactor = 1;
+	[SerializeField] private float gravity = 9.8f;
+	[SerializeField] private Vector2 veloc;
+	[SerializeField] private float jumpForce = 1;
+	
+	
+	[SerializeField] private GameObject groundCheck;
+	private bool grounded;
+
+	private enum PlayerState
+	{
+		Idle, Crouch, Hang, Run, Fall
+	}
+	private PlayerState currentState;
+	
 	
     // Start is called before the first frame update
     void Start()
@@ -20,12 +30,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //  analyze left/right input
-        int horzInput = 0;
-        horzInput += Convert.ToInt32(Input.GetKey("right"));
-        horzInput -= Convert.ToInt32(Input.GetKey("left"));
-        //	apply lerp: accel/decel
-		veloc.x = Mathf.Lerp(veloc.x, horzInput, Time.deltaTime);
+	    GroundCheck();
+	    switch (currentState)
+	    {
+		    case PlayerState.Idle:
+			    
+			    MoveCheck(0);
+			    break;
+		    case PlayerState.Crouch:
+			    break;
+		    
+	    }
+        
 		
 		//	jump
 		// if(/* we're on the ground or on an edge */) {
@@ -51,5 +67,28 @@ public class PlayerMovement : MonoBehaviour
         //  update position
 		transform.position += new Vector3(veloc.x, veloc.y, 0);
 		
+    }
+
+    private void MoveCheck(float horzInput)
+    {
+	    //  analyze left/right input
+	    
+	    //	apply lerp: accel/decel
+	    veloc.x = Mathf.Lerp(veloc.x, horzInput, Time.deltaTime);
+    }
+    private void GroundCheck()
+    {
+	    RaycastHit2D hit = Physics2D.Raycast(groundCheck.transform.position, Vector2.down);
+	    if (hit.collider != null)
+	    {
+		    float groundDistance = Mathf.Abs(hit.point.y - groundCheck.transform.position.y);
+	    }
+    }
+
+    private void InputChecks()
+    {
+	    float horzInput = 0;
+	    horzInput += Convert.ToInt32(Input.GetKey("right"));
+	    horzInput -= Convert.ToInt32(Input.GetKey("left"));
     }
 }
