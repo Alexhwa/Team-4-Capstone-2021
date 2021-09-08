6bff83fc3ebddb1a15399c0f2529ce223418013d
Alexhwa <ah.alswad@gmail.com>
2021-09-08 14:37:24 -0500

    Stealth section

---
+++ Outgrown/Assets/Scripts/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    public string loadedLevel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(loadedLevel);
+        }
+    }
+}
