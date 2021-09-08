fa8faf86067bb17bacd6686cea4466226c05e954
Alexhwa <ah.alswad@gmail.com>
2021-09-08 05:03:02 -0500

    Level 1

---
+++ Outgrown/Assets/Scripts/DampCamera2D.cs
@@ -0,0 +1,40 @@
+﻿// Smooth towards the target
+
+using System;
+using UnityEngine;
+using System.Collections;
+     
+public class DampCamera2D : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float delayFollow;
+    public bool following;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        if (delayFollow > 0)
+        {
+            StartCoroutine(StartFollowing(delayFollow));
+        }
+    }
+
+    private IEnumerator StartFollowing(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        following = true;
+    }
+    void Update()
+    {
+        if (following)
+        {
+            // Define a target position above and behind the target transform
+            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10)) + offset;
+
+            // Smoothly move the camera towards that target position
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothTime);
+        }
+    }
+}
