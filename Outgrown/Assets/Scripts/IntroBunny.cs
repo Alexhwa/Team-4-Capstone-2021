ce556b97ba569ba3af2d3b3524e89822fa8c5db7
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2021-09-22 14:28:48 -0500

    Spikes and initial bunny structure

---
+++ Outgrown/Assets/Scripts/IntroBunny.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroBunny : MonoBehaviour
+{
+	// Path[] paths;
+	int movePhase;
+	
+    void Start(){
+		movePhase = 0;
+		
+	}
+    
+	void Update(){
+		//	increase Timer by Time.deltaTime * moveSpeed
+	}
+	
+	//	
+	void OnTriggerEnter2D(Collider2D collider) {
+		movePhase++;
+	}
+	
+	//	Insert method to remove bunny from level here
+}
