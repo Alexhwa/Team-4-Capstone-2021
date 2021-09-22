2a76cd323727a5f09df89912fa4336cad33bd81a
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2021-09-22 14:50:29 -0500

    Bunny behavior draft

--- Outgrown/Assets/Scripts/IntroBunny.cs
+++ Outgrown/Assets/Scripts/IntroBunny.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class IntroBunny : MonoBehaviour
 {
-	// Path[] paths;
-	int movePhase;
+	//	each path has a series of checkpoints
+	Path[] paths;
+	private bool allowTrigger;
+	private int movePhase;
+	private int moveSpeed;
+	private Vector3 nextLocation;
 	
     void Start(){
 		movePhase = 0;
-		
+		moveSpeed = 0;
 	}
     
-	void Update(){
-		//	increase Timer by Time.deltaTime * moveSpeed
+	void Update() {
+		/*
+		if(!allowTrigger) {
+			move towards next location
+			if(reach it || nextLocation == null) {
+				change nextLocation to next position in path[movePhase]
+				moveSpeed = nextLocation[2];
+				if(no next position) {
+					allowTrigger = true;
+				}
+			}
+		}
+		*/
 	}
 	
 	//	
 	void OnTriggerEnter2D(Collider2D collider) {
-		movePhase++;
+		if(allowTrigger) {
+			movePhase++;
+			nextLocation = paths[movePhase].getCheckpoint(0);
+			allowTrigger = false;
+		}
 	}
 	
 	//	Insert method to remove bunny from level here
