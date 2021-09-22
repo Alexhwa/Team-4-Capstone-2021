d587fc24ccd748f7f6469bf37947dcf98e0360fd
RemusW <rwong2100@hotmail.com>
2021-09-21 21:23:49 -0500

    added checkpoint system with player death

---
+++ Outgrown/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private Preloaded preloader;
+
+    float playerHealth = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void damagePlayer(float damage)
+    {
+        if (damage >= playerHealth)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            preloader = GameObject.FindGameObjectWithTag("Preloader").GetComponent<Preloaded>();
+            transform.position = preloader.lastCheckpointPos;
+        }
+        else
+        {
+            playerHealth -= damage;
+        }
+    }
+}
--- Outgrown/Assets/Scripts/Preloaded.cs
+++ Outgrown/Assets/Scripts/Preloaded.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Preloaded : MonoBehaviour
 {
+    private static Preloaded instance;
+
+    public Vector3 lastCheckpointPos;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+            print("destroying");
+        }
     }
 
     // Update is called once per frame
