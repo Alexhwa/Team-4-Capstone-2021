8f41d56736475abf6fce7259028f2a4e429f3c73
Alexhwa <ah.alswad@gmail.com>
2021-10-19 02:21:47 -0500

    spider wake up animation

---
+++ Outgrown/Assets/Scripts/Spider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Spider : MonoBehaviour
+{
+    public Transform bodyTransform;
+    public Vector2 raycastDirection;
+    public float minDistAway;
+    [Range(0f,1f)]
+    public float bodyYAdjustSpeed;
+    public LayerMask groundMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastToGround();
+    }
+
+    private void RaycastToGround()
+    {
+        
+        RaycastHit2D hit = Physics2D.Raycast(bodyTransform.position,raycastDirection, 10000, groundMask);
+        if (hit.collider != null)
+        {
+            if (Vector3.Distance(hit.point, bodyTransform.position) < minDistAway)
+            {
+                print("raycast threshold reached");
+                Vector3 newYLoc = hit.point;
+                newYLoc.x = bodyTransform.position.x;
+                newYLoc.y += minDistAway;
+                bodyTransform.position = Vector3.Lerp(bodyTransform.position, newYLoc, bodyYAdjustSpeed);
+            }
+            else
+            {
+                Vector3 newYLoc = hit.point;
+                newYLoc.x = bodyTransform.position.x;
+                newYLoc.y += minDistAway;
+                bodyTransform.position = Vector3.Lerp(bodyTransform.position, newYLoc, bodyYAdjustSpeed);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(bodyTransform.position, raycastDirection);
+    }
+}
