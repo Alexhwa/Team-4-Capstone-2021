2d374b5dbc4c28dc75f5ecb6fcb1b473a35b1437
RemusW <rwong2100@hotmail.com>
2021-10-20 15:52:42 -0500

    added cranes in background and level1 design changes

---
+++ Outgrown/Assets/Scripts/Interaction/CraneAI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneAI : MonoBehaviour
+{
+    Vector3 horizontalDir = Vector3.right;
+    float startHeight; // 138-540
+    Vector3 left;
+    Vector3 right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void Awake()
+    {
+        float scale = Random.Range(0.02573985f, 0.07189397f);
+        gameObject.transform.localScale += new Vector3(scale, scale, scale);
+        Vector2 size = GetComponent<SpriteRenderer>().size;
+        GetComponent<SpriteRenderer>().size.Set(size.x, Random.Range(400, 1200));
+        print(GetComponent<SpriteRenderer>().size);
+        startHeight = Random.Range(138, 540);
+    }
+
+    public void InitializeValues(Vector3 dir, Vector3 left, Vector3 right)
+    {
+        horizontalDir = dir;
+        this.left = left;
+        this.right = right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (horizontalDir == Vector3.right)
+        {
+            transform.Translate(Vector3.right * 4 * Time.deltaTime);
+            if (transform.position.x >= right.x)
+                Destroy(gameObject);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * 4 * Time.deltaTime);
+            if (transform.position.x <= left.x)
+                Destroy(gameObject);
+        }
+        // transform.position = Vector3.MoveTowards(start, end, 3 * Time.deltaTime);
+    }
+}
---
+++ Outgrown/Assets/Scripts/Interaction/CraneBackground.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneBackground : MonoBehaviour
+{
+    [SerializeField] GameObject crane_sprite;
+    [SerializeField] GameObject left_spawn;
+    [SerializeField] GameObject right_spawn;
+    List<GameObject> cranes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cranes = new List<GameObject>();
+        StartCoroutine("SpawnNextCrane");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator SpawnNextCrane()
+    {
+        while (true)
+        {
+            // cranes.Add(Instantiate<GameObject>(crane_sprite));
+            int rng = Random.Range(-1, 1);
+            Vector3 dir;
+            Vector3 position;
+            print(rng);
+            if (rng >= 0)
+            {
+                position = (left_spawn.transform.position);
+                dir = Vector3.right;
+            }
+            else
+            {
+                position = (right_spawn.transform.position);
+                dir = Vector3.left;
+            }
+            CraneAI crane = Instantiate(crane_sprite, position, Quaternion.identity, gameObject.transform).GetComponent<CraneAI>();
+            crane.InitializeValues(dir, left_spawn.transform.position, right_spawn.transform.position);
+            yield return new WaitForSeconds(Random.Range(20, 30));
+
+        }
+    }
+}
