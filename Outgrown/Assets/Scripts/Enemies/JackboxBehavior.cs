a7e49b6797a344ec11526c1659eb20d6d77eae4a
RemusW <rwong2100@hotmail.com>
2021-10-27 17:41:09 -0500

    jackbox bounce

--- Outgrown/Assets/Scripts/Enemies/JackboxBehavior.cs
+++ Outgrown/Assets/Scripts/Enemies/JackboxBehavior.cs
@@ -9,20 +9,39 @@ public class JackboxBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        print("jacbox boo!");
         if (collision.transform.tag == "Player")
         {
             animator.SetBool("eventTriggered", true);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("jacbox boo!");
+        if (other.tag == "Player")
+        {
+
+            animator.SetBool("eventTriggered", true);
+            StartCoroutine(waitforseconds());
+            gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator waitforseconds()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
     }
 }
