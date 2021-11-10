a60d74fd5c6aef690351c1b7dc8e2a4f0bb86a6e
RemusW <rwong2100@hotmail.com>
2021-11-09 19:00:43 -0600

    soldier

---
+++ Outgrown/Assets/Scripts/Stealth/SoldierLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class SoldierLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        print("triggeer");
+
+        if (collider.tag == "Player")
+        {
+            if (!collider.GetComponent<PlayerCover>().InCover())
+            {
+                gameObject.GetComponent<Light2D>().color = Color.red;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeath>().damagePlayer(1);
+            }
+        }
+    }
+}
