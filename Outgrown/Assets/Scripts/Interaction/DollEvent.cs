eda24e209ddcf2aba98a8c873aa7e7ebca1f76cf
RemusW <rwong2100@hotmail.com>
2021-11-10 17:44:34 -0600

    added fog layering and doll arm retrieval

---
+++ Outgrown/Assets/Scripts/Interaction/DollEvent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollEvent : MonoBehaviour
+{
+    [SerializeField] GameObject DollMissingArm;
+    [SerializeField] GameObject rope;
+    [SerializeField] Vector3 ropeFinalPos = new Vector3(0,0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Doll Arm")
+        {
+            DollMissingArm.SetActive(true);
+            Destroy(collision.gameObject);
+            StartCoroutine(MoveFunction());
+        }
+    }
+
+    public void FixDoll()
+    {
+        // Doll.GetComponent<SpriteRenderer>().sprite = repairedDoll;
+        DollMissingArm.SetActive(true);
+        MoveFunction();
+    }
+
+    IEnumerator MoveFunction()
+    {
+        print("movefunction");
+        float timeSinceStarted = 0f;
+        float lerpDuration = 3;
+        while (timeSinceStarted < lerpDuration)
+        {
+            rope.transform.position = Vector3.Lerp(rope.transform.position, ropeFinalPos, timeSinceStarted / lerpDuration);
+            timeSinceStarted += Time.deltaTime * .05f;
+
+            // Otherwise, continue next frame
+            yield return null;
+        }
+    }
+}
