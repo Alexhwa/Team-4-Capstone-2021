c2f5b22c80b5eaec645e6b892679cd1eae5719ab
Alexhwa <ah.alswad@gmail.com>
2021-11-17 14:33:15 -0600

    end UI

---
+++ Outgrown/Assets/Scripts/ReturnToTitle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class ReturnToTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.anyKey.isPressed)
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
---
+++ Outgrown/Assets/Scripts/UITextTypeWriter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using UnityEngine.UI;
+
+// attach to UI Text component (with the full text already there)
+
+public class UITextTypeWriter : MonoBehaviour
+{
+
+    public float typeSpeed = 10;
+    
+    private Text txt;
+    private string story;
+
+    public UnityEvent onTextEnd = new UnityEvent();
+
+    void Awake () 
+    {
+        txt = GetComponent<Text> ();
+        story = txt.text;
+        txt.text = "";
+
+        // TODO: add optional delay when to start
+        StartCoroutine ("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story) 
+        {
+            if (c == '~')
+            {
+                //do nothing
+            }
+            else
+            {
+                txt.text += c;
+            }
+
+            yield return new WaitForSeconds(1f / typeSpeed);
+        }
+        onTextEnd.Invoke();
+    }
+
+}
