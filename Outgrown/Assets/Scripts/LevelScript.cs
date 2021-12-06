1323cbff226f912d0c0ea0e93887177d26032082
Alexhwa <ah.alswad@gmail.com>
2021-12-05 21:16:29 -0600

    new random sounds and footsteps

--- Outgrown/Assets/Scripts/LevelScript.cs
+++ Outgrown/Assets/Scripts/LevelScript.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class LevelScript : MonoBehaviour
 {
     public AudioClip levelMusic;
+    public AudioClip[] randomSounds;
+    public float randomSoundInterval;
+    public float randomSoundChance;
     // Start is called before the first frame update
     void Start()
     {
         AudioManager.Instance.PlayMusic(levelMusic);
+        if (randomSounds.Length > 0)
+        {
+            StartCoroutine(TryPlayRandomSound());
+        }
+    }
+
+    private IEnumerator TryPlayRandomSound()
+    {
+        yield return new WaitForSeconds(randomSoundInterval + Random.Range(0, randomSoundInterval));
+        if (Random.Range(0f, 1f) < randomSoundChance)
+        {
+            AudioManager.Instance.PlaySfx(randomSounds[Random.Range(0, randomSounds.Length)]);
+        }
+
+        StartCoroutine(TryPlayRandomSound());
     }
 }
--- Outgrown/Assets/Scripts/Spikes.cs
+++ Outgrown/Assets/Scripts/Spikes.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-//    void Start(){}
-
-//    void Update() {}
+	public AudioClip deathSound;
 	
 	void OnCollisionEnter2D(Collision2D collision) {
 		GameObject that = collision.gameObject;
 		var PDscript = that.GetComponent<PlayerDeath>();
 		if(PDscript != null) {
 			PDscript.damagePlayer(1);
-			print("LOL YOU FELL INTO A SPIKES");	
+			print("LOL YOU FELL INTO A SPIKES");
+
+			if (deathSound)
+			{
+				AudioManager.Instance.PlaySfx(deathSound);
+			}
 		}
 	}
 }
