feafb6467cffbfa5cfc7489a242eeb75ba08a3c6
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2021-12-06 11:20:18 -0600

    Jack in the box at various parts in the level, bunny still needs death

---
+++ Outgrown/Assets/Scripts/Enemies/JackboxEndBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using TMPro;
+using Yarn.Unity;
+using Yarn.Unity.Example;
+
+public class JackboxEndBehavior : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject bgsprites;
+    [SerializeField] GameObject dialogueEvent;
+    [SerializeField] DialogueRunner dialogueRunner;
+    [SerializeField] NPC npc;
+    int spriteShown = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("jacbox boo! (Collision)");
+        if (collision.transform.tag == "Player")
+        {
+            animator.SetBool("eventTriggered", true);
+  	    dialogueEvent.SetActive(true);
+ 	    dialogueRunner.StartDialogue(npc.talkToNode);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("jacbox boo! (trigger)");
+        if (other.tag == "Player")
+        {
+            animator.SetBool("eventTriggered", true);
+  	    dialogueEvent.SetActive(true);
+ 	    dialogueRunner.StartDialogue(npc.talkToNode);
+            StartCoroutine(waitforseconds());
+            gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator waitforseconds()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+    }
+
+    [YarnCommand("ShowSprite")]
+    public void ShowSprite() {
+        GameObject[] go = bgsprites.GetComponentsInChildren<GameObject>();
+	go[spriteShown].SetActive(true);
+	spriteShown++;
+    }
+}
