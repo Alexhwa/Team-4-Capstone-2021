11e53705b5e781a8762ff8806ec7c534c8f27e8e
Alexhwa <ah.alswad@gmail.com>
2021-12-06 06:02:31 -0600

    ending cutscene added

---
+++ Outgrown/Assets/Scripts/Cutscene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    public bool cutsceneDone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cutsceneDone)
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
